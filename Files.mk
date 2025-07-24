54960d2c677576baf57a4ff2ba43baf408c1b1d6
dotacow <ykitaneh22@gmail.com>
2025-07-25 00:51:53 +0300

    rgb validation left

--- Files.mk
+++ Files.mk
@@ -2,6 +2,8 @@ RENDER= render.c
 
 #EXIT= exit/utils.c
 
-PARSER= parsing/validate_map.c parsing/utils.c parsing/found_utils.c
+PARSER= parsing/found_utils.c parsing/validate_textures.c \
+	parsing/validate_map.c \
+	parsing/utils.c parsing/found_utils.c
 
 FILES = main.c $(PARSER) #$(EXIT) #$(RENDER)
