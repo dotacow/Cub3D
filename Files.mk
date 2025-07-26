754897ea2e15c01fc2b3c8fd000d9130385817ab
dotacow <dotacowidkmynamesry@gmail.com>
2025-07-26 18:57:57 +0300

    path fixes

--- Files.mk
+++ Files.mk
@@ -1,9 +1,10 @@
 RENDER= render.c
 
+INIT=
+
 #EXIT= exit/utils.c
 
 PARSER= parsing/found_utils.c parsing/validate_textures.c \
-	parsing/validate_map.c \
-	parsing/utils.c parsing/found_utils.c
+		parsing/validate_map.c parsing/utils.c parsing/validate_rgb.c
 
-FILES = main.c $(PARSER) #$(EXIT) #$(RENDER)
+FILES = main.c $(PARSER) #$(INIT) #$(EXIT) #$(RENDER)
