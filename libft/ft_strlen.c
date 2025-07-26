3558a2f5b5231b4cadf78b9882996aa7fe0932ec
dotacow <dotacowidkmynamesry@gmail.com>
2025-07-26 18:48:38 +0300

    fixed strlen xd

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dotacow <dotacow@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 12:51:23 by yokitane          #+#    #+#             */
-/*   Updated: 2024/11/28 23:23:03 by dotacow          ###   ########.fr       */
+/*   Updated: 2025/07/26 18:24:12 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	int	i;
 
 	i = 0;
-	while (s[i] != '\0' && s[i] != ' ')
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
