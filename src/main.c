264e2bfa4b73ac15e2453563e2d8df11bdb806b2
dotacow <dotacowidkmynamesry@gmail.com>
2025-07-26 18:58:11 +0300

    adding main tester

--- src/main.c
+++ src/main.c
@@ -6,57 +6,25 @@
 /*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 19:56:37 by yokitane          #+#    #+#             */
-/*   Updated: 2025/07/24 22:21:20 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/07/26 18:55:00 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Written by Bruh
-
 #include "../includes/cub3d.h"
 
-#define WIDTH 256
-#define HEIGHT 256
-
-// Exit the program as failure.
-static void ft_error(void)
+int	main(int argc, char **argv)
 {
-	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-	exit(EXIT_FAILURE);
-}
+	int	fd;
+	// t_map_elements map;
 
-// Print the window width and height.//
-static void ft_hook(void* param)
-{
-	const mlx_t* mlx = param;
-
-	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
-}
-
-
-int32_t	main(int ac, char **av)
-{
-
-	// MLX allows you to define its core behaviour before startup.
-	if (!validate_args(ac, av))
+	if (argc != 2 || !validate_args(argc, argv))
 		return (0);
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "hello world ;p", false);
-	if (!mlx)
-		ft_error();
-
-	/* Do stuff */
-	// Create and display the image.
-	mlx_image_t* img = mlx_new_image(mlx, 256, 256);
-	if (!img || (mlx_image_to_window(mlx, img, 0, 0) < 0))
-		ft_error();
-
-	// Even after the image is being displayed, we can still modify the buffer.
-	mlx_put_pixel(img, 0, 0, 0xFF0000FF);
-
-	// Register a hook and pass mlx as an optional param.
-	// NOTE: Do this before calling mlx_loop!
-	mlx_loop_hook(mlx, ft_hook, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx);
-	return (EXIT_SUCCESS);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (1);
+	is_valid_map(fd);
+	//map = init_map(fd);
+	//cast_thy_rays(map);
+	//loops_hooks and mlx stuff
+	return (0);
 }
