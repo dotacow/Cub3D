8c4f3b106bf2d540cfe4cfe48439216ad98b2660
dotacow <ykitaneh22@gmail.com>
2025-08-19 16:06:43 +0300

    libft optimizations and fixes

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 11:01:37 by yokitane          #+#    #+#             */
-/*   Updated: 2024/08/30 15:33:25 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/08/19 15:49:28 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	tmpdest = dest;
 	tmpsrc = src;
-	while (n--)
+	while (n-- > 0)
 		tmpdest[n] = tmpsrc[n];
 	return (dest);
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,29 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yokitane <yokitane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yokitane <yokitane@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 11:54:05 by yokitane          #+#    #+#             */
-/*   Updated: 2024/08/29 14:52:06 by yokitane         ###   ########.fr       */
+/*   Updated: 2025/08/19 16:06:19 by yokitane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t i = 0;
 
-	i = 0;
-	if (size == 0)
+	if (size > 0)
 	{
-		return (ft_strlen(src));
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	while (src[i] != '\0' && i < size - 1)
-	{
-		dst[i] = src[i];
+	while (src[i] != '\0')
 		i++;
-	}
-	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (i);
 }
+
